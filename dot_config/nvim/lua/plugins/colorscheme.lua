f6adf1e47fabd81b44260008445dfea48c4309fe
soothsayerwally <krishangnayanbhagawati@gmail.com>
2025-03-24 16:32:52 +0530

    change nvim theme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  "neanias/everforest-nvim",
+  "catppuccin/nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd("colorscheme everforest")
+    vim.cmd("colorscheme catppuccin-mocha")
   end,
 }
