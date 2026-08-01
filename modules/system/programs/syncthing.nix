d8c315589ce7ba0ef55a8ecf6322fc1f9cbdccf5
Krishang <krishangnayanbhagawati@gmail.com>
2026-08-02 00:55:22 +0530

    setup syncthing

--- modules/system/programs/syncthing.nix
+++ modules/system/programs/syncthing.nix
@@ -2,5 +2,8 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true; # Open ports in the firewall for Syncthing. (NOTE: this will not open syncthing gui port)
+    user = "krishang";
+    configDir = "/home/krishang/.config/syncthing";
   };
 }
+
