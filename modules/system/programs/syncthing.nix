f7ef657bb8c2d736663b17de8e57de05b68e27a9
Krishang <krishangnayanbhagawati@gmail.com>
2026-08-02 00:22:18 +0530

    fix syntax error

--- modules/system/programs/syncthing.nix
+++ modules/system/programs/syncthing.nix
@@ -1,4 +1,6 @@
-services.syncthing = {
-  enable = true;
-  openDefaultPorts = true; # Open ports in the firewall for Syncthing. (NOTE: this will not open syncthing gui port)
-};
+{ ... }: {
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true; # Open ports in the firewall for Syncthing. (NOTE: this will not open syncthing gui port)
+  };
+}
