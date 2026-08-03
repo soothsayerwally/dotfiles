bd62a1b864757a6e57a27a5b9221272eb55278c9
Krishang <krishangnayanbhagawati@gmail.com>
2026-08-03 23:48:07 +0530

    fix steam cursor, add distrobox

--- modules/system/programs/steam.nix
+++ modules/system/programs/steam.nix
@@ -3,9 +3,13 @@
     enable = true;
     remotePlay.openFirewall = true; #Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports for Source Dedicated Server hosting
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+    extraPackages = with pkgs; [
+      adwaita-icon-theme
+    ];
   };
+
   programs.gamemode.enable = true;
-  programs.steam.extraCompatPackages = with pkgs; [
-  proton-ge-bin
-  ];
 }
