4456372b71cb6299b046aef055dbe3e2d8f1257c
Krishang <krishangnayanbhagawati@gmail.com>
2026-08-03 12:45:16 +0530

    pkgs.fishPlugins.fzf-fish -> pkgs.fishPlugins.fzf

--- modules/home/programs/fish.nix
+++ modules/home/programs/fish.nix
@@ -5,7 +5,7 @@
       set fish_greeting # Disable greeting
     '';
     plugins = [
-    { name = "fzf-fish"; pkgs.fishPlugins.fzf-fish; } 
+    { name = "fzf-fish"; pkgs.fishPlugins.fzf; } 
     { name = "colored-man-pages"; pkgs.fishPlugins.colored-man-pages; }
     { name = "tide"; pkgs.fishPlugins.tide; }
     { name = "done"; pkgs.fishPlugins.done; }
