037a33d826204687b41612e2f4afe76d7cd94dc0
Krishang <krishangnayanbhagawati@gmail.com>
2026-08-03 12:47:41 +0530

    fix yet another embarrassing error

--- modules/home/programs/fish.nix
+++ modules/home/programs/fish.nix
@@ -5,10 +5,10 @@
       set fish_greeting # Disable greeting
     '';
     plugins = [
-    { name = "fzf-fish"; pkgs.fishPlugins.fzf; } 
-    { name = "colored-man-pages"; pkgs.fishPlugins.colored-man-pages; }
-    { name = "tide"; pkgs.fishPlugins.tide; }
-    { name = "done"; pkgs.fishPlugins.done; }
+    { name = "fzf-fish"; src = pkgs.fishPlugins.fzf; } 
+    { name = "colored-man-pages"; src = pkgs.fishPlugins.colored-man-pages; }
+    { name = "tide"; src = pkgs.fishPlugins.tide; }
+    { name = "done"; src = pkgs.fishPlugins.done; }
     ];
   };
 }
