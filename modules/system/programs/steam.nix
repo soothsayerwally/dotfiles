89a31fe1635b092167e7ab8128dfe538ac0691fa
Krishang <krishangnayanbhagawati@gmail.com>
2026-08-18 13:03:32 +0530

    disable steam

--- modules/system/programs/steam.nix
+++ modules/system/programs/steam.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; #Open ports in the firewall for Steam Remote Play
+    enable = false;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports for Source Dedicated Server hosting
     extraCompatPackages = with pkgs; [
       proton-ge-bin
