863be074c9f7e20418fe83db2cd4bb022ab9502a
Krishang <krishangnayanbhagawati@gmail.com>
2026-08-20 20:58:35 +0530

    enable steam

--- modules/system/programs/steam.nix
+++ modules/system/programs/steam.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   programs.steam = {
-    enable = false;
+    enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports for Source Dedicated Server hosting
     extraCompatPackages = with pkgs; [
